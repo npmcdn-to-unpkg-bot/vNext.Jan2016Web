4c4f6be870f6c28cd42de6115df357e2648599d7
ghstahl@gmail.com <ghstahl@gmail.com>
2016-02-23 08:06:20 -0800

    added serilog

--- src/Pingo.Filters/AutofacModule.cs
+++ src/Pingo.Filters/AutofacModule.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 using Autofac;
 using Microsoft.AspNet.Mvc.Filters;
 using Pingo.Core.Reflection;
+using Serilog;
 using Module = Autofac.Module;
 
 namespace Pingo.Filters
 {
     public class AutofacModule : Module
     {
+        static ILogger logger = Log.ForContext<AutofacModule>();
         protected override void Load(ContainerBuilder builder)
         {
+            logger.Information("Hi from pingo.filters Autofac.Load!");
             var assembly = this.GetType().GetTypeInfo().Assembly;
             var derivedTypes = TypeHelper<ActionFilterAttribute>.FindDerivedTypes(assembly).ToArray();
+            var derivedTypesName = derivedTypes.Select(x => x.GetTypeInfo().Name);
+            logger.Information("Found these types: {DerivedTypes}", derivedTypesName);
+
             builder.RegisterTypes(derivedTypes);
             /*
 
