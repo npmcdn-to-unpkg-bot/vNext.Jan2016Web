ba8d02baca2b8dacc92d07b2852cb4d6de430719
Herb <ghstahl@gmail.com>
2016-03-11 14:10:39 -0800

    returning a ChallengeResult from within our AuthActionFilter. This is the Identity Way of doing things.

--- src/Pingo.Filters/AuthActionFilter.cs
+++ src/Pingo.Filters/AuthActionFilter.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Filters;
-using Microsoft.AspNet.Routing;
-using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Security.Claims;
 
@@ -9,32 +7,11 @@ namespace Pingo.Filters
 {
     public class AuthActionFilter : ActionFilterAttribute
     {
-        public static string Area { get; set; }
-        public static string Controller { get; set; }
-        public static string Action { get; set; }
-        private static IConfigurationRoot _configurationRoot;
-        public AuthActionFilter(IConfigurationRoot configurationRoot)
-        {
-            _configurationRoot = configurationRoot;
-            Area = _configurationRoot["Filters:Configuration:AuthActionFilter:Area"];
-            Controller = _configurationRoot["Filters:Configuration:AuthActionFilter:Controller"];
-            Action = _configurationRoot["Filters:Configuration:AuthActionFilter:Action"];
-        }
-
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.HttpContext.User.Identity.IsAuthenticated)
             {
-                var rtrr = new RouteValueDictionary(
-                    new
-                    {
-                        action = Action,
-                        controller = Controller,
-                        area = Area,
-                        ReturnUrl = context.HttpContext.Request.Path
-                    });
-
-                context.Result = new RedirectToRouteResult(rtrr);
+                context.Result =  new ChallengeResult();
             }
             else
             {
